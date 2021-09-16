d26cbfd3b0a57b9d05cf400ba48a36846c50beb7
niqmarin <niqmarin@gmail.com>
2021-09-16 21:25:37 +0300

    lesson 3: initialize buttons and text views

--- app/src/main/java/ru/gb/androidstart/calculator/MainActivity.java
+++ app/src/main/java/ru/gb/androidstart/calculator/MainActivity.java
@@ -3,12 +3,62 @@ package ru.gb.androidstart.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button zeroButton;
+    private Button oneButton;
+    private Button twoButton;
+    private Button threeButton;
+    private Button fourButton;
+    private Button fiveButton;
+    private Button sixButton;
+    private Button sevenButton;
+    private Button eightButton;
+    private Button nineButton;
+    private Button plusButton;
+    private Button minusButton;
+    private Button multiplyButton;
+    private Button divideButton;
+    private Button reverseButton;
+    private Button percentButton;
+    private Button clearButton;
+    private ImageButton backspaceButton;
+    private TextView inputTextView;
+    private TextView lastOperationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+
+    }
+
+    public void initializeViews() {
+        inputTextView = findViewById(R.id.input_text_view);
+        lastOperationTextView = findViewById(R.id.last_operation_text_view);
+        zeroButton = findViewById(R.id.zero_button);
+        oneButton = findViewById(R.id.one_button);
+        twoButton = findViewById(R.id.two_button);
+        threeButton = findViewById(R.id.three_button);
+        fourButton = findViewById(R.id.four_button);
+        fiveButton = findViewById(R.id.five_button);
+        sixButton = findViewById(R.id.six_button);
+        sevenButton = findViewById(R.id.seven_button);
+        eightButton = findViewById(R.id.eight_button);
+        nineButton = findViewById(R.id.nine_button);
+        plusButton = findViewById(R.id.plus_button);
+        minusButton = findViewById(R.id.minus_button);
+        multiplyButton = findViewById(R.id.multiply_button);
+        divideButton = findViewById(R.id.divide_button);
+        reverseButton = findViewById(R.id.reverse_button);
+        percentButton = findViewById(R.id.percent_button);
+        backspaceButton = findViewById(R.id.backspace_button);
+        clearButton = findViewById(R.id.clear_all_button);
     }
 }
